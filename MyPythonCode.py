545b03267d0d7808e88ac8a423a6e323bf59d175
Ghost171717 <ghost40031717@gmail.com>
2024-09-23 22:36:57 -0600

    Add files via upload

---
+++ MyPythonCode.py
@@ -0,0 +1,10 @@
+name = input("Give me your admin name: ")
+ID = input("Enter your DOB :")
+age = input("Enter your age :")
+role = input("Enter your role :")
+
+print(f"\nAdmin User")
+print(f"name: {name}")
+print(f"ID: {ID}")
+print(f"age: {age}")
+print(f"role: {role}")
